ccdf8f04203df998e5be248d6177807f7aaff62f
Ciaran Kieran <ciaranjohnkieran@gmail.com>
2026-01-04 03:48:17 +0000

    Update settings.py

--- backend/mulletbackend/mulletbackend/settings.py
+++ backend/mulletbackend/mulletbackend/settings.py
@@ -90,12 +90,17 @@ WSGI_APPLICATION = 'mulletbackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3",  # local fallback
+        conn_max_age=600,
+        ssl_require=bool(os.environ.get("RENDER")),
+    )
 }
+
 
 
 # Password validation
