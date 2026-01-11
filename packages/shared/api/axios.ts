cfc80d41d7f8de7cdcc991aa94f430566b58307b
Ciaran Kieran <ciaranjohnkieran@gmail.com>
2026-01-11 01:30:36 +0000

    Update axios.ts

--- packages/shared/api/axios.ts
+++ packages/shared/api/axios.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NEXT_PUBLIC_API_URL
+    ? `${process.env.NEXT_PUBLIC_API_URL.replace(/\/$/, "")}/api`
+    : "http://localhost:8000/api"; // fallback for local dev
+
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL,
   withCredentials: true,
   xsrfCookieName: "csrftoken",
   xsrfHeaderName: "X-CSRFToken",
